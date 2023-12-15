78a7fd105786f012ea55f067a4c5612c9a0ff88b
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-12-16 00:19:58 +0530

    add: solution 1436

---
+++ solutions/1436. Destination City/Solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        s = {a for a, _ in paths}
+        return next(b for _, b in paths if b not in s)
