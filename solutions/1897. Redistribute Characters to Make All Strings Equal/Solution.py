8a1ea4d18f16f9f8c1c211ae05c3b5e6a6f48d4e
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-12-31 21:33:11 +0530

    add: solution 1897

---
+++ solutions/1897. Redistribute Characters to Make All Strings Equal/Solution.py
@@ -0,0 +1,12 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        counter = Counter()
+        for word in words:
+            for c in word:
+                counter[c] += 1
+        n = len(words)
+        return all(count % n == 0 for count in counter.values())
