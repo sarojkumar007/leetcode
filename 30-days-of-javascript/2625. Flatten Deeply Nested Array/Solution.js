89ba958e613abd9d89d9afe17804d6294fd2d244
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2024-01-04 13:35:24 +0530

    add: js solution 2625

---
+++ 30-days-of-javascript/2625. Flatten Deeply Nested Array/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+  if (n <= 0) return arr;
+
+  const ans = [];
+  for (const x of arr) {
+    if (Array.isArray(x)) {
+      ans.push(...flat(x, n - 1));
+    } else {
+      ans.push(x);
+    }
+  }
+  return ans;
+};
