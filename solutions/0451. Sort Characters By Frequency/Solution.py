529bd78c61f0f8b922a3b1c2d3e796fc7cdbeaeb
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2024-02-21 11:53:46 +0530

    add: solution 0451

---
+++ solutions/0451. Sort Characters By Frequency/Solution.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        cnt = Counter(s)
+        return ''.join(c * v for c, v in sorted(cnt.items(), key=lambda x: -x[1]))
