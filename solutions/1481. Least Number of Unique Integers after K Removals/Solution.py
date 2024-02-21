0e63a49df7ce5798dd72549b667647a5d1ebb0e4
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2024-02-21 12:41:33 +0530

    add: solution 1481

---
+++ solutions/1481. Least Number of Unique Integers after K Removals/Solution.py
@@ -0,0 +1,12 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        cnt = Counter(arr)
+        for i, v in enumerate(sorted(cnt.values())):
+            k -= v
+            if k < 0:
+                return len(cnt) - i
+        return 0
