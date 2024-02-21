3c3d62d1626e136950dcc941f2fe0ed04a6d5aa7
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2024-02-21 13:01:45 +0530

    add: solution 0268

---
+++ solutions/0268. Missing Number/Solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = int(( n * ( n + 1 ) ) / 2)
+
+        return total - sum(nums)
